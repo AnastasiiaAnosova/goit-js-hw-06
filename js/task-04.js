4e3f5f4264757261db5ecc6c4e1f03f9c1113d20
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-06-29 22:02:56 +0300

    update task 4

--- js/task-04.js
+++ js/task-04.js
@@ -3,16 +3,15 @@ const counter = document.querySelector('#value');
 
 
 const decrement = document.querySelector('[data-action="decrement"]');
-decrement.addEventListener("click", decrementCounter);
-//() => {
-//     counterValue -= 1;
-//     counter.textContent = counterValue;
-// });
-
-const decrementCounter = () => {
+decrement.addEventListener("click", () => {
     counterValue -= 1;
     counter.textContent = counterValue;
-}
+});
+
+// const decrementCounter = () => {
+//     counterValue -= 1;
+//     counter.textContent = counterValue;
+// }
 
 const increment = document.querySelector('[data-action="increment"]');
 increment.addEventListener("click",() => {
