9cf208e9c8bb9929ee4950cbf350ae1ed2938476
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-07-01 22:52:59 +0300

    task 9 finished

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+console.dir(body);
+
+const btnChangeColor = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+btnChangeColor.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  const newColor = getRandomHexColor();
+
+  // body.style.backgroundColor = newColor;
+  // colorName.textContent = newColor;
+
+  document.body.style.backgroundColor = newColor;
+  colorName.textContent = newColor;
+}
