75eb6fb5ffb47d769bafd41aaa03b980ac9bdf30
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-07-05 23:17:07 +0300

    fixed task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const categoriesList = document.querySelectorAll('.item');
-console.log('Number of categories:${categoriesList.length}');
+console.log(`Number of categories:${categoriesList.length}`);
 categoriesList.forEach(element => { 
-    console.log('Category:${element.firstChild.textContent}');
-    console.log('Elements:${element.lastElementChild.children.length}');
+    console.log(`Category:${element.firstElementChild.textContent}`);
+    console.log(`Elements:${element.lastElementChild.children.length}`);
 })
